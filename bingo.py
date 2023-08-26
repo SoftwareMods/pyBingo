07bb464b9c2c7476c0c0e06de01f48ccc6190560
Software-Mods.com <scott.m.rowley@gmail.com>
2023-08-26 15:45:19 -0500

    Moved mask_image to helpers

--- bingo.py
+++ bingo.py
@@ -107,58 +107,6 @@ class MainWindow(QMainWindow):
         widget.setLayout(home_page)
         self.setCentralWidget(widget)
 
-    def mask_image(self, imgdata, imgtype="png", size=64):
-        # Load image
-        image = QImage.fromData(imgdata, imgtype)
-
-        # convert image to 32-bit ARGB (adds an alpha
-        # channel ie transparency factor):
-        image.convertToFormat(QImage.Format_ARGB32)
-
-        # Crop image to a square:
-        imgsize = min(image.width(), image.height())
-        rect = QRect(
-            int((image.width() - imgsize) / 2),
-            int((image.height() - imgsize) / 2),
-            imgsize,
-            imgsize,
-        )
-
-        image = image.copy(rect)
-
-        # Create the output image with the same dimensions
-        # and an alpha channel and make it completely transparent:
-        out_img = QImage(imgsize, imgsize, QImage.Format_ARGB32)
-        out_img.fill(Qt.transparent)
-
-        # Create a texture brush and paint a circle
-        # with the original image onto the output image:
-        brush = QBrush(image)
-
-        # Paint the output image
-        painter = QPainter(out_img)
-        painter.setBrush(brush)
-
-        # Don't draw an outline
-        painter.setPen(Qt.NoPen)
-
-        # drawing circle
-        painter.drawEllipse(0, 0, imgsize, imgsize)
-
-        # closing painter event
-        painter.end()
-
-        # Convert the image to a pixmap and rescale it.
-        pr = QWindow().devicePixelRatio()
-        pm = QPixmap.fromImage(out_img)
-        pm.setDevicePixelRatio(pr)
-        size *= pr
-        size = int(size)
-        pm = pm.scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-
-        # return back the pixmap data
-        return pm
-
     def show_about(self):
         self.setStyleSheet("")
 
@@ -176,7 +124,7 @@ class MainWindow(QMainWindow):
         imgdata = open(imgpath, "rb").read()
 
         # calling the function
-        pixmap = self.mask_image(imgdata, size=200)
+        pixmap = mask_image(imgdata, size=200)
 
         # creating label
         self.developer_img = QLabel("Image Label")
